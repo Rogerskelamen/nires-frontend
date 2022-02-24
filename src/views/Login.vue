310117b357ad996f97824579db5e22c39dff0ea5
Rogerskelamen <rogerskelamen@gmail.com>
2022-02-24 20:29:37 +0800

    design promot

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,24 @@
 <template>
-  <div>
-    
+  <div id="login">
+
   </div>
 </template>
 
 <script>
 export default {
-  
+  data () {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      }
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
-  
+#login {
+  background: url('https://gitee.com/rogerskelamen/mdpic/raw/master/img/20220224174106.png') no-repeat cover;
+}
 </style>
